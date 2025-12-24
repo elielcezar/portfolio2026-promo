5a49ae453fe11dfdc6a6a1857540ad1574f1d511
Eliel Cezar <elielcezar@gmail.com>
2025-12-24 10:58:11 -0300

    feat: define esquema de dcores nos itens de portfolio

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -15,15 +15,36 @@ export default function Header() {
           <h1>ElielCezar</h1>     
         </div>
         <nav>
-          <ul>
+          <ul>           
             <li>
-              <Link href="/">Home</Link>
+              <Link href="/portfolio">Portfolio</Link>
             </li>
             <li>
-              <Link href="/sobre">Sobre</Link>
+              <Link href="/about">Sobre</Link>
             </li>
             <li>
-              <Link href="/contato">Contato</Link>
+              <Link href="/services">Serviços</Link>
+            </li>            
+            <li>
+              <Link href="/contact">Contato</Link>
+            </li>
+            <li>
+              <Link href="https://github.com/elielcezar">
+                <Image src="/icon-github.svg"
+                       alt="GitHub"
+                       width={24}
+                       height={24}
+                       className="icon-github-image" />
+              </Link>
+            </li>
+            <li>
+              <Link href="https://www.linkedin.com/in/elielcezar/">
+                <Image src="/icon-linkedin.svg"
+                       alt="LinkedIn"
+                       width={24}
+                       height={24}
+                       className="icon-linkedin-image" />
+              </Link>
             </li>
           </ul>
         </nav>   
