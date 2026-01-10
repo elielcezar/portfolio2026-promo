a6d633f28441329c27f9525d9755c5d5a0f93272
Eliel Cezar <elielcezar@gmail.com>
2026-01-10 14:23:35 -0300

    fix: ajuste de build

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -49,7 +49,7 @@ export default function Hero() {
 
             <Link href="/portfolio" onClick={(event) => handleLinkClick(event, 'portfolio')} className="link">
 
-              <svg fill="#000000" width="48" height="48" viewBox="0 0 24 24" fill="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
+              <svg fill="#000000" width="48" height="48" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg">
                 <title>down</title>
                 <path d="M11.125 16.313l7.688-7.688 3.594 3.719-11.094 11.063-11.313-11.313 3.5-3.531z"></path>
               </svg>
